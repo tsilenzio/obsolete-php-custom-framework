0578116eec3c35e491200368a200cf1ee978c6f7
Taylor Silenzio <me@tsilenz.io>
2018-04-19 01:17:01 -0400

    starting the autoloader, the first part of a good framework (without composer that is)

--- public/index.php
+++ public/index.php
@@ -1,2 +1,12 @@
 <?php
 
+/**
+ * Custom Framework - A simple project to help me dust off my PHP skills
+ * 
+ * @author Taylor Silenzio <me@tsilenz.io>
+ */
+
+ # Keep track of app cpu time
+define('APP_START', microtime(true));
+
+require __DIR__ . '/../bootstrap/autoloader.php';
